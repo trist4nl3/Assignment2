4b85eaa0955ff197da4aea1c546055d6b2839748
trist4nl3 <s4008982@student.rmit.edu.au>
2024-05-15 17:33:51 +1000

    Finish the implementation of question 3

--- binary_addition.asm
+++ binary_addition.asm
@@ -1,108 +1,120 @@
 .ORIG x3000
-; Write an LC-3 that reads two 16-bit binary words from the Minecraft world and then adds them together
-; Read first number using the same logic for question 2.
-; R0 -> X player position / Number to convert
-; R1 -> Y player position / quotient
-; R2 -> Z Player position / remainder
-; R3 -> Temp value
-; R4 -> Iterator
-; R5 -> Temp Quotient for the MC API
-; R6 -> Temp Remainder API
-; R7 -> The players Z position
-; Converting the first number to binary and in game
+; Reading the first number
+JSR READ_BINARY
+ST R6, FIRST_NUMBER ; Storing the first number
+LD R0, CURRENT_Z ; Increasing Z
+ADD R0, R0, #1
+ST R0, CURRENT_Z
+; Reading the second number
+JSR READ_BINARY
+ST R6, SECOND_NUMBER ; Storing second number
+LD R0, CURRENT_Z ; Increasing Z
+ADD R0, R0, #1
+ST R0, CURRENT_Z
 
-; Resetting all registers to be used
-AND R1, R1, #0
-AND R2, R2, #0
-AND R3, R3, #0
-AND R4, R4, #0
-AND R5, R5, #0
-AND R6, R6, #0
-LD R0, FIRST_NUMBER_TO_CONVERT
-JSR BINARY_LOOP
-LD R7, ZPOSITION
-ADD R7, R7, #1
-ST R7, ZPOSITION
-LD R0, SECOND_NUMBER_TO_CONVERT
-JSR BINARY_LOOP
-LD R7, ZPOSITION
-ADD R7, R7, #1
-ST R7, ZPOSITION
-; Getting the binary addition of the two numbers
-; Resetting the first 3 values
+; Getting the total for the write binary addition
 AND R0, R0, #0
-AND R1, R1, #0
-AND R2, R2, #0
-LD R1, FIRST_NUMBER_TO_CONVERT
-LD R2, SECOND_NUMBER_TO_CONVERT
-ADD R0, R1, R2
-; Resetting the other registers
-AND R1, R1, #0
-AND R2, R2, #0
-JSR BINARY_LOOP
+LD R0, FIRST_NUMBER
+LD R1, SECOND_NUMBER
+ADD R0, R0, R1
+JSR WRITE_BINARY_ADDITION
+;TRAP 0x36
 HALT
-
-; First loop for the first binary function
-BINARY_LOOP
-    ADD R0, R0, #0 ; enable comparisions
-    ; calling the subroutine
-    ; R1 -> The quotient
-    ; R2 -> The remainder
-    JSR DIVIDEBY2
-    ; Temp storage of the quotient
-    AND R5, R5, #0
-    AND R5, R1, #0
-    ; Temp storage of the remainder
+; R5 -> Be the iterator from 1-16
+; R5 -> Be the base that doubles for every iteration
+; R6 -> Be the total that is added to calculate the binary
+READ_BINARY
+    ; Getting Player Position, stored in R0, R1, R2
+    TRAP 0x31
+    ; Resetting needed regisiters
     AND R6, R6, #0
-    AND R6, R2, #0
-    ; Using the temp value with the iterator
-    AND R3, R3, #0 ; Resetting R3 
-    AND R3, R4, #-16 ; Checks if its more or less than 16 used for validation + block placing
-    BRz ENDLOOP ; if R4 - 16 = 0 then it ends
-    ADD R4, R4, #1 ; i++
-    ; Resetting R0-3 for the minecraft TRAP
-    AND R0, R0, #0
+    ; Setting up iterator
+    AND R4, R4, #0
+    ADD R4, R4, #1
+    LD R5, CURRENT_Z
+    ; Adjusting to start reading the binary
+    ADD R0, R0, #1
+    ADD R2, R2, R5
+    ; This should start reading from [player_pos_x + 1, player_pos_y, player_pos_z + current_z]
+    ; Resetting R5 so it can be used and setting it up to be doubled
+    AND R5, R5, #0
+    ADD R5, R5, #1
+
+    read_loop
+        AND R3, R3, #0
+        ADD R3, R4, #-16
+        BRzp EXIT
+        TRAP 0x33 ; Stores the block id in R3
+        ADD R3, R3, #0 ; enable comparisons
+        BRz ISZERO
+        ADD R6, R6, R5
+        ISZERO
+        ADD R5, R5, R5
+        ADD R4, R4, #1
+        ADD R0, R0, #1
+        
+        BR read_loop
+    EXIT
+    RET
+            
+WRITE_BINARY_ADDITION
+    ; Resetting registers
     AND R1, R1, #0
     AND R2, R2, #0
-    ; Getting player position
-    TRAP 0x31
-    ADD R0, R0, R4 ; Adding to the players x position
-    AND R7, R7, #0 ; Resetting R7
-    LD R7, ZPOSITION
-    ADD R2, R2, R7 ; Adding to the players z position
+    AND R4, R4, #0
+    ; Binary loop for each iteration take the remainder as this is the least bit which we output in minecraft
+    LOOP_1 
+        ADD R0, R0, #0 ; enable comparisions
+        ; calling the subroutine
+        ; R1 -> The quotient
+        ; R2 -> The remainder
+        AND R1, R1, #0 ; prepping quotient
+        AND R2, R2, #0 ; prepping remainder
+        AND R3, R3, #0 ; prepping temp
+        ADD R3, R0, #0 ; setting temp = x
+        ; loops until it divides by 2
+        div_loop
+            ADD R3, R3, #-2 ; dividing by 2
+            BRn EXIT_LOOP_2 ; exits if the temp is negative
+            ADD R1, R1, #1 ; adding to the quotient
+            BRzp div_loop
+        EXIT_LOOP_2
+        ADD R2, R3, #2 ; fixing up some issues here
+        ; Storing temp values for quotient and remainder to work with the traps
+        ; Quotient
+        AND R5, R5, #0
+        ADD R5, R1, #0
+        ; Remainder
+        AND R6, R6, #0
+        ADD R6, R2, #0
+        TRAP 0x36
+        ; using temp for the iterator
+        AND R3, R3, #0
+        ADD R3, R4, #-16
+        BRz ENDLOOP
+        ADD R4, R4, #1
+        ; This will reset R0, R1, R2
+        AND R0, R0, #0
+        AND R1, R1, #0
+        AND R2, R2, #0
+        ; Getting the players position and storing them in their respective registers
+        TRAP 0x31
+        ADD R0, R0, R4 ; adding to the players x position 
+        LD R3, CURRENT_Z
+        ADD R2, R2, R3
+        AND R3, R3, #0 ; resetting R3 Value
+        ADD R3, R3, R6; Making the block ID whatever the remainder is
+        TRAP 0x34 ; Placing the blocks
+        ; Resetting and setting values for the loop
+        AND R0, R0, #0
+        ADD R0, R5, #0 ; setting the current x value to quotient
+        BRnzp LOOP_1
 
-    AND R3, R3, #0 ; Resetting R3 value 
-    AND R3, R3, R6 ; Making the block ID whatever the remainder is
-    TRAP 0x34 ; placing the blocks
-    ; Resetting and setting values for the loop
-    AND R0, R0, #0
-    AND R0, R5, #0
-    BRnzp BINARY_LOOP
-ENDLOOP
-RET
+    ENDLOOP
+    RET
 
-
-DIVIDEBY2
-    AND R1, R1, #0 ; Prepping quotient
-    AND R2, R2, #0 ; Prepping remainder
-    AND R3, R3, #0 ; Prepping the temp value
-    ADD R3, R0, #0 ; Let temp = x
-    ; Loops until it divides by 2
-    div_loop
-        ADD R3, R3, #-2 ; dividing by 2
-        BRn EXIT ; exits if the temp is negative
-        ADD R1, R1, #1
-        BRzp div_loop
-
-EXIT
-    ADD R2, R3, #2 ; Adds 2 to the quotient to make it balanaced
-
-
-RET
-
-
-FIRST_NUMBER_TO_CONVERT .FILL #229
-SECOND_NUMBER_TO_CONVERT .FILL #297
-ZPOSITION .FILL #0
-
+CURRENT_Z .FILL #1
+FIRST_NUMBER .FILL #0
+SECOND_NUMBER .FILL #0
+THIRD_NUMBER .FILL #0
 .END
